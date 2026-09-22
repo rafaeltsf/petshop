d6a33c40f50b2e45724ef3c9604e01ab6b16e2b6
ratoleproso <annecaroly.78@gmail.com>
2026-09-22 17:58:59 -0300

    tela de cadastro

--- back-end/server.js
+++ back-end/server.js
@@ -36,29 +36,4 @@ app.post('/api/login', async (req, res) => {
   }
 });
 
-app.post('/api/register', async (req, res) => {
-  const { email, senha, name } = req.body;
-
-  if (!email || !senha) {
-    return res.status(400).json({ message: 'E-mail e senha são obrigatórios.' });
-  }
-
-  try {
-    const user = await prisma.user.create({
-      data: { email, password: senha, name }
-    });
-
-    const { password, ...usuarioSemSenha } = user;
-
-    res.status(201).json({ message: 'Cadastro realizado com sucesso!', usuario: usuarioSemSenha });
-
-  } catch (error) {
-    if (error.code === 'P2002') {
-      return res.status(409).json({ message: 'Este e-mail já está cadastrado.' });
-    }
-    console.error(error);
-    res.status(500).json({ message: 'Erro no servidor.' });
-  }
-});
-
 app.listen(3000, () => console.log('Servidor rodando na porta 3000'));
